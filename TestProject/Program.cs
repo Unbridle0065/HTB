2bc2925c5b046f0ce2d8446b84215f777bf1ff95
Unbridle0065 <cliffordsolomon@gmail.com>
2023-11-13 15:02:03 -0600

    Initial commit of .NET project for RCE testing

---
+++ TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Executing test command...");
+        Process process = new Process();
+        process.StartInfo.FileName = "cmd.exe";
+        process.StartInfo.Arguments = "/c dir"; // This is a Windows command to list directory contents
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.Start();
+
+        Console.WriteLine(process.StandardOutput.ReadToEnd());
+        process.WaitForExit();
+    }
+}
